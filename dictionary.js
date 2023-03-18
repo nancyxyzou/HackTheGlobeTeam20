04d51c00857ead4912c7b5e46a4cdd954823d74f
plutoot <128110727+plutoot@users.noreply.github.com>
2023-03-18 10:02:04 -0400

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,32 @@
+const medicalTerms = [
+    {term: 'Allergies', definition: 'An abnormal reaction of the immune system to a harmless substance.'},
+    {term: 'Angina', definition: 'Chest pain or discomfort that you feel when there is not enough blood flowing to your heart.'},
+    {term: 'Asthma', definition: 'A long-term (or chronic) disease that leads the airways to your lungs to swell, making it hard to breathe.'},
+    {term: 'Anaesthetic', definition: 'A drug that reduces pain or that puts you to sleep as during surgery.'},
+    
+    {term: 'Blood Thinners', definition: 'M[edicines that help blood flow smoothly without blood clots.'},
+    
+    {term: 'Deep Vein Thrombosis', definition: 'A blood clot that forms in a vein deep in your body.'},
+    {term: 'DVT', definition: 'A blood clot that forms in a vein deep in your body.'},
+    {term: 'Dialysis', definition: 'A treatment that does the work of your kidneys when they can\'t.'},
+
+    {term: 'Latex Allergy', definition: 'An abnormal reaction of the immune system to natural, rubber latex.'},
+
+    {term: 'Hypertension', definition: 'High blood pressure'},
+
+    {term: 'Malignant Hyperthermia', definition: 'A severe reaction to certain drugs used to reduce pain or put you to sleep (ie. anesthesia).'},
+    {term: 'Medication', definition: 'Medicine; substances used to treat, manage, or prevent medical conditions.'},
+    
+    {term: 'Physician', definition: 'A doctor.'},
+    {term: 'Pulmonary Embolism', definition: 'When a blood clot blocks in an artery in your lung suddenly.'},
+    
+    {term: 'Referring Doctor', definition: 'Another doctor who redirected you to get a second opinion.'},
+    
+    {term: 'Sleep Apnea', definition: 'When you stop breathing for 10 seconds or more when you sleep.'},
+    
+    {term: 'Thyroid', definition: 'A gland in your neck that is shaped like a butterfly. It makes hormones that help your body work normally.'},
+    
+    {term: 'Version Code', definition: '2-letter code at the end of the health number on an Ontario Health Card.'},
+    
+    {term: 'WSIB', definition: 'Workplace Safety and Insurance Board'},
+]
